ca161ea2ec46f7ba9522dde1d7f4b5b5d004d4f7
wedens <wedens13@yandex.ru>
2012-11-03 21:29:52 +0700

    added association user-*>article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Article do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  before { @article = user.articles.build(title: "Article title",
+                                          content: "Lorem ipsum")}
+
+  subject { @article }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  its(:user) { should == user }
+
+  it { should be_valid}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(username: "user1") }
+
+  subject { @user }
+
+  it { should respond_to(:username) }
+  it { should respond_to(:articles) }
+
+  it { should be_valid }
 end
