909720ca0be77fd364675284d673c0bae4b1ca10
wedens <wedens13@yandex.ru>
2012-11-03 20:10:24 +0700

    Initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Antarcticle::Application.config.secret_token = '7d2ba01d9553de8feea38367e38bf941d7f193add8d682c01a1f39e7acef4e9d967cbf806979434a6383625b91b207865a8f9121ac228fe9f8530b2b7994250c'
