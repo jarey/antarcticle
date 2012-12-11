eb6cebc7e823e8895ba54e567dd1c5060cf9c888
wedens <wedens13@yandex.ru>
2012-12-11 18:47:21 +0700

    spec refactored

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,12 +2,8 @@ require 'spec_helper'
 
 describe ArticlesController do
 
-  def attributes
-    { "title" => "title", "content" => "content" }
-  end
-
-  let(:article) { mock_model('Article') }
-  let(:current_user) { mock_model('User') }
+  let(:attributes) { { "title" => "title", "content" => "content" } }
+  let(:article) { mock_model(Article).as_null_object }
   let(:article_id) { article.to_param }
 
   before  do
@@ -15,38 +11,39 @@ describe ArticlesController do
     @ability.extend(CanCan::Ability)
     @ability.stub(:can?).and_return(true)
     controller.stub(:current_ability).and_return(@ability)
+    Article.stub(:find).with(article_id).and_return(article)
   end
 
   describe "#update" do
-
-    before do
-      Article.should_receive(:find).with(article_id).and_return(article)
+    it "authorizes update" do
       controller.should_receive(:authorize!).with(:update, article)
+      put :update, id: article_id
+    end
+
+    it "tries to update article" do
+      article.should_receive(:update_attributes)
+      put :update, id: article_id
     end
 
     context "when attributes are valid" do
-      before do
-        article.should_receive(:update_attributes).and_return(true)
-      end
+      before { put :update, id: article_id }
 
       it "redirects to article" do
-        put :update, id: article_id
         should redirect_to(article)
       end
 
-      it "contains notice" do
-        put :update, id: article_id
+      it "sets notice" do
         flash[:notice].should_not be_blank
       end
     end
 
     context "when attributes are not valid" do
       before do
-        article.should_receive(:update_attributes).and_return(false)
+        article.stub(:update_attributes).and_return(false)
+        put :update, id: article_id
       end
 
       it "re-renders edit method" do
-        put :update, id: article_id
         should render_template(:edit)
       end
     end
@@ -54,59 +51,75 @@ describe ArticlesController do
 
   describe "#create" do
     let(:articles) { mock('articles') }
+    let(:current_user) { mock_model(User) }
 
     before do
-      controller.should_receive(:authorize!).with(:create, Article)
       controller.stub(:current_user).and_return(current_user)
+      current_user.stub(:articles).and_return(articles)
+      articles.stub(:build).with(attributes).and_return(article)
+    end
+
+    it "authorizes creation" do
+      controller.should_receive(:authorize!).with(:create, Article)
+      post :create, article: attributes
+    end
+
+    it "creates new article as current user" do
       current_user.should_receive(:articles).and_return(articles)
       articles.should_receive(:build).with(attributes).and_return(article)
+      post :create, article: attributes
+    end
+
+    it "tries to save created article" do
+      article.should_receive(:save)
+      post :create, article: attributes
     end
 
     context "when attributes are valid" do
-      before do
-        article.should_receive(:save).and_return(true)
-      end
+      before { post :create, article: attributes }
 
       it "redirects to article" do
-        post :create, article: attributes
         should redirect_to(article)
       end
 
-      it "contains notice" do
-        post :create, article: attributes
+      it "sets notice message" do
         flash[:notice].should_not be_blank
       end
     end
 
     context "when attributes are not valid" do
       before  do
-        article.should_receive(:save).and_return(false)
+        article.stub(:save).and_return(false)
+        post :create, article: attributes
       end
 
       it "re-renders new method " do
-        post :create, article: attributes
         should render_template(:new)
       end
     end
   end
 
   describe "#show" do
-    before do
-      Article.should_receive(:find).with(article_id).and_return(article)
+    it "authorizes show" do
       controller.should_receive(:authorize!).with(:read, article)
+      get :show, id: article_id
     end
 
-    it "assigns article" do
+    it "assigns @article" do
       get :show, id: article_id
       assigns(:article).should be article
     end
   end
 
   describe "#destroy" do
-    before do
-      Article.should_receive(:find).with(article_id).and_return(article)
-      controller.should_receive(:authorize!).with(:destroy, article)
+    it "destroys article" do
       article.should_receive(:destroy)
+      delete :destroy, id: article_id
+    end
+
+    it "authorizes destroy" do
+      controller.should_receive(:authorize!).with(:destroy, article)
+      delete :destroy, id: article_id
     end
 
     it "redirects to articles index" do
@@ -116,12 +129,12 @@ describe ArticlesController do
   end
 
   describe "#edit" do
-    before do
-      Article.should_receive(:find).with(article_id).and_return(article)
+    it "authorizes edit" do
       controller.should_receive(:authorize!).with(:update, article)
+      get :edit, id: article_id
     end
 
-    it "assigns article" do
+    it "assigns @article" do
       get :edit, id: article_id
       assigns(:article).should be article
     end
@@ -131,11 +144,20 @@ describe ArticlesController do
     let(:new_article) { mock('article') }
 
     before do
-      controller.should_receive(:authorize!).with(:create, Article)
-      Article.should_receive(:new).and_return(new_article)
+      Article.stub(:new).and_return(new_article)
     end
 
-    it "assigns article" do
+    it "authorizes new" do
+      controller.should_receive(:authorize!).with(:create, Article)
+      get :new
+    end
+
+    it "creates new article" do
+      Article.should_receive(:new).with(no_args)
+      get :new
+    end
+
+    it "assigns @article" do
       get :new
       assigns(:article).should be new_article
     end
@@ -146,8 +168,17 @@ describe ArticlesController do
     let(:page) { "1" }
 
     before do
+      Article.stub(:get_page).and_return(articles)
+    end
+
+    it "reads paginated result" do
+      Article.should_receive(:get_page).with(page)
+      get :index, { page: page }
+    end
+
+    it "authorizes read" do
       controller.should_receive(:authorize!).with(:read, Article)
-      Article.should_receive(:get_page).with(page).and_return(articles)
+      get :index, { page: page }
     end
 
     it "assigns articles" do
