6f4b9ea3a5e51197b19317f72bd3b54f4b9bb982
wedens <wedens13@yandex.ru>
2013-07-23 23:27:21 +0700

    #ANTARCTICLE-164 ability to run app with MRI

--- lib/jndi_properties.rb
+++ lib/jndi_properties.rb
@@ -1,23 +1,38 @@
-require 'java'
-import javax.naming.InitialContext
+if RUBY_PLATFORM == "java"
+  require 'java'
+  import javax.naming.InitialContext
 
-module JndiProperties
-  def self.getProperty(name)
-    begin
-      env.lookup(name).to_s
-    rescue
-      nil
+  module JndiProperties
+    def self.getProperty(name)
+      begin
+        env.lookup(name).to_s
+      rescue
+        nil
+      end
+    end
+
+    def self.[](name)
+      getProperty(name)
+    end
+    private
+
+    def self.env
+      context = InitialContext.new
+      environment = context.lookup 'java:comp/env'
+      environment
     end
   end
 
-  def self.[](name)
-    getProperty(name)
-  end
-  private
 
-  def self.env
-    context = InitialContext.new
-    environment = context.lookup 'java:comp/env'
-    environment
+else
+
+  module JndiProperties
+    def self.getProperty(name)
+      nil
+    end
+
+    def self.[](name)
+      getProperty(name)
+    end
   end
 end
