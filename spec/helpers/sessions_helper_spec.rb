77a3c78e0e0fe4535737151d056592342215f07a
wedens <wedens@eldis.ru>
2013-07-24 16:03:50 +0700

    added remember me cookie test

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -11,5 +11,19 @@ describe SessionsHelper do
       current_user?(@user).should be_true
       signed_in?.should be_true
     end
+
+    it "should set remember me cookie" do
+      cookies[:remember_token].should be @user.remember_token
+    end
+  end
+
+  describe "sign out" do
+    it "should sign out user" do
+      signed_in?.should be_false
+    end
+
+    it "should unset remember me cookie" do
+      cookies[:remember_token].should be nil
+    end
   end
 end
