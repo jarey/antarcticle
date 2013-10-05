d100ae62486af4362994f3f3671ba9f5704560d1
sbashkyrtsev <sbashkyrtsev@griddynamics.com>
2013-10-05 13:07:29 +0400

    Added an assets precompilation so that it does not fail during startup with "ActionView::Template::Error (application.css isnt precompiled)". Also have added defaults for database password so taht it is easier to run the app

--- build-war.sh
+++ build-war.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # compile assets
-RAILS_ENV=development rake assets:clean && RAILS_RELATIVE_URL_ROOT=/antarcticle RAILS_ENV=development rake assets:precompile
+RAILS_ENV=development rake assets:clean && rake assets:precompile && RAILS_RELATIVE_URL_ROOT=/antarcticle RAILS_ENV=development rake assets:precompile
 
 # build war
 RAILS_ENV=production warble
